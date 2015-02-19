125ef231f785efc3fe9968b29111fbbcdb52ab3e
wangjun2012al <wangjun2012al@gmail.com>
2015-02-19 18:27:00 +0800

    + add Regexp_attribute.js

---
+++ RegExp_sttribute.js
@@ -0,0 +1,12 @@
+var parttern_str=/cat/igm;
+var parttern_fun=new RegExp("cat","igm");
+console.log(parttern_str.ignoreCase);
+console.log(parttern_str.global);
+console.log(parttern_str.source);
+console.log(parttern_str.multiline);
+console.log(parttern_str.lastIndex);
+console.log(parttern_fun.ignoreCase);
+console.log(parttern_fun.global);
+console.log(parttern_fun.source);
+console.log(parttern_fun.multiline);
+console.log(parttern_fun.lastIndex);
