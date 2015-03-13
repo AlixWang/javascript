53e8a1f42e6c7d5041846a87e3028955107768ca
wangjun2012al <wangjun2012al@gmail.com>
2015-03-13 22:16:32 +0800

    * netserver.js

--- nodejs/netserver.js
+++ nodejs/netserver.js
@@ -3,17 +3,33 @@ chat.on('connection',function (client){
     client.name=client.remoteAddress+":"+client.remotePort;
     client.write("HI"+client.name+"!\n");
     clientList.push(client);
+    
     client.on('data',function(data){
         broadcast(data,client);
     });
+
+    client.on('end',function(){
+    clientList.splice(clientList.indexOf(client), 1);
+    });
+
 });
 
 function broadcast(message,client){
-    for(var i=0;i<clientList.length;i++){
+    var cleanup=[];    
+    for(var i=0;i<clientList.length;i++){            
         if(client!==clientList[i]){
-        clientList[i].write(client.name+"says"+message);
-        }
+            if(clientList[i].writable){
+                
+                clientList[i].write(client.name+"says"+message);
+
+            }else{
+            cleanup.push(clientList[i]);
+            clientList[i].destroy();
+            }
     }
 }
- 
+    for(i=0;i<cleanup.length;i+=1) {
+            clientList.splice(clientList.indexOf(cleanup[i]), 1)
+    }
+} 
 chat.listen(8000);
