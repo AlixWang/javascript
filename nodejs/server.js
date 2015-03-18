3f59aed6ee86ef5b95abe31b2f164353ea12c3b7
wangjun2012al <wangjun2012al@gmail.com>
2015-03-18 21:18:08 +0800

    * change something to merge origin

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,7 @@
-#!/usr/local/bin/node
 var http=require("http");
-http.createServer(function(request,response){
-    response.writeHead(200,{"Content-Type":"text/plain","charset":"utf-8" ,"Content-Length":"20"});
-    response.end('<p>hello my world<p>');
+http.createServer(function (req,rsp){
+    rsp.writeHead(200,{"Content-Type":"text/html"});
+    rsp.write("<p>this is text node app</p>");
+    rsp.end();
 }).listen(8080);
-console.log("server setup");
-
+console.log("server is set up");
