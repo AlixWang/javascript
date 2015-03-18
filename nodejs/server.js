feb23d6d266b60e1bf186cb7fc26e14de2c10977
wangjun2012al <wangjun2012al@gmail.com>
2015-03-18 14:40:35 +0800

    merge someshting

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,8 +4,4 @@ http.createServer(function (req,rsp){
     rsp.write("<p>this is text node app</p>");
     rsp.end();
 }).listen(8080);
-<<<<<<< HEAD
 console.log("server is set up");
-=======
-console.log("server setup");
->>>>>>> fae25085c1565b0259e00bba960192dd07841a52
