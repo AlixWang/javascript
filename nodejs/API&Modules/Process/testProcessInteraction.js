90b89622a625f1f13b59b51e8eb67c93155edde4
AlixWang <wangjun2012al@gmail.com>
2015-03-26 22:56:04 +0800

    + add some file for process and test them on linux platform

---
+++ nodejs/API&Modules/Process/testProcessInteraction.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @authors Alix Wang (wangjun2012al@gmail.com)
+ * @date    2015-03-26 22:48:31
+ * @version $Id$
+ */
+ //interaction to the node process
+ /*
+  * process.version 	//this will return the node version
+  *	process.installPrefix	//this will return the node install directory
+  * process.platform	//this will return the platform of opration systeam
+  * process.uptime	//this will return this node process running time till now
+  *
+ */
+
+ console.log(process.version);
+ console.log(process.installPrefix);
+ console.log(process.platform);
+ console.log(process.uptime());
