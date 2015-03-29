417a66d3486e26f7b70b7772d8c602246063cc85
AlixWang <wangjun2012al@gmail.com>
2015-03-29 23:45:43 +0800

    + add vm modules

---
+++ nodejs/API&Modules/VM/testVm.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @authors Alix Wang (wangjun2012al@gmail.com)
+ * @date    2015-03-29 23:26:37
+ * @version $Id$
+ */
+
+ //vm moudles
+ var vm = require('vm');
+
+ var resault = vm.runInThisContext('1+1');
+ console.log(resault);
